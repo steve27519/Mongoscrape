d2c67b34adcc16cd083e4050e9fa4f32f3e2e318
steve27519 <stevenyurchuck@gmail.com>
2018-07-03 15:17:16 -0400

    initial

---
+++ routes/article_contoller.js
@@ -0,0 +1,73 @@
+$("#scrape").on("click", function() {
+    $.ajax({
+        method: "GET",
+        url: "/scrape",
+    }).done(function(data) {
+        console.log(data)
+        window.location = "/"
+    })
+});
+
+$(".navbar-nav li").click(function() {
+   $(".navbar-nav li").removeClass("active");
+   $(this).addClass("active");
+});
+
+//Saving Article
+$(".save").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/save/" + thisId
+    }).done(function(data) {
+        window.location = "/"
+    })
+});
+
+//Deleting
+$(".delete").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/delete/" + thisId
+    }).done(function(data) {
+        window.location = "/saved"
+    })
+});
+
+//Saving note
+$(".saveNote").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    if (!$("#noteText" + thisId).val()) {
+        alert("please enter a note to save")
+    }else {
+      $.ajax({
+            method: "POST",
+            url: "/notes/save/" + thisId,
+            data: {
+              text: $("#noteText" + thisId).val()
+            }
+          }).done(function(data) {
+              
+              console.log(data);
+              // Clearing Note
+              $("#noteText" + thisId).val("");
+              $(".modalNote").modal("hide");
+              window.location = "/saved"
+          });
+    }
+});
+
+// Delete Note button
+$(".deleteNote").on("click", function() {
+    var noteId = $(this).attr("data-note-id");
+    var articleId = $(this).attr("data-article-id");
+    $.ajax({
+        method: "DELETE",
+        url: "/notes/delete/" + noteId + "/" + articleId
+    }).done(function(data) {
+        console.log(data)
+        $(".modalNote").modal("hide");
+        window.location = "/saved"
+    })
+});
